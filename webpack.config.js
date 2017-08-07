2a1d2b278fb56fad285a89c0b0dd8067e12d2073
Sean <sean@layer3tv.com>
2017-08-07 12:38:51 -0600

    updated webpack config to minify and added build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 module.exports = {
   entry: [
     './src/index.js'
@@ -19,6 +20,16 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  ],
   devServer: {
     historyApiFallback: true,
     contentBase: './'
